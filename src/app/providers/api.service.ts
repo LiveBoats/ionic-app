d7e7b431ffa9ff468c87fd5b227e1441fbca930b
Matthieu Vion <mvion@trace-software.com>
2017-05-18 14:04:02 +0200

    better componenents structure
    Fixed observable import
    Updated dependencies

--- src/app/providers/api.service.ts
+++ src/app/providers/api.service.ts
@@ -4,7 +4,7 @@
 
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { Observable } from "rxjs/Observable";
+import { Observable } from "rxjs/Rx";
 import { Boat } from "../models/boat";
 
 @Injectable()
